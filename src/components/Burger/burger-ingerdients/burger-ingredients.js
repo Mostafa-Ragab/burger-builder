3bfaeef06136d3b6e4eec1b404575e95bc35927a
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-06 17:45:31 +0200

    addinbg burger css

---
+++ src/components/Burger/burger-ingerdients/burger-ingredients.js
@@ -0,0 +1,33 @@
+import './burger-ingredients'
+const BurgerIngredient = (props) => {
+    let ingerdient = null;
+
+    switch(props.type){
+        case ('bread-bottom'):
+            ingerdient = <div className="BreadBottom"></div>
+            break;
+        case ('bread-top'):
+            ingerdient=(<div className="BreadTop">
+                <div className="Seeds1"></div>
+                <div className="Seeds2"></div>
+            </div>);
+            break;
+        case ('meat'):
+            ingerdient = <div className="Meat"></div> 
+            break;
+        case ('cheese'):
+            ingerdient = <div className="Cheese"></div> 
+            break;
+        case ('bacon'):
+            ingerdient = <div className="Bacon"></div> 
+            break;
+        case ('salad'):
+            ingerdient = <div className="Salad"></div> 
+            break;
+        default:
+           ingerdient = null;
+    };
+
+}
+
+export default BurgerIngredient

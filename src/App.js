f6cb269889cf739c0551028a387eab4586205485
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-09 21:55:25 +0200

    adding burgerBuilder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Layout from "./components/layout/Layout";
+import BurgerBuilder from "./containers/burger-builder/burger-builder";
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Layout>
+				<BurgerBuilder />
+			</Layout>
+		</div>
+	);
 }
 
 export default App;

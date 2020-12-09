14543c5ec4b156f8b58d70130075fca46ab35ce4
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-09 21:55:42 +0200

    adding Layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,9 @@
-import './layout.scss'
-import Aux from '../../hoc/Aux_js'
+import "./layout.scss";
+import Aux from "../../hoc/Aux_js";
 
 const Layout = (props) => (
-    <Aux>
-        <main className="content">
-            {PaymentResponse.children}
-        </main>
-    </Aux>
-)
-export default Layout
+	<Aux>
+		<main className="content">{PaymentResponse.children}</main>
+	</Aux>
+);
+export default Layout;

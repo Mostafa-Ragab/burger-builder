037f458c6ff97d652a88b9a13cbc2ff140c4e975
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-09 23:32:54 +0200

    fixing bugs in reduce func

--- src/containers/burger-builder/burger-builder.js
+++ src/containers/burger-builder/burger-builder.js
@@ -3,12 +3,16 @@ import React, { Component } from "react";
 import Burger from "../../components/burger/Burger";
 
 class BurgerBuilder extends Component {
+	// constructor(props) {
+	//     super(props);
+	//     this.state = {...}
+	// }
 	state = {
 		ingredients: {
-			salad: 0,
-			bacon: 0,
-			cheese: 0,
-			meat: 0,
+			salad: 1,
+			bacon: 1,
+			cheese: 2,
+			meat: 2,
 		},
 	};
 

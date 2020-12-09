520115412d21a6e72026ab855e78de438f21bf32
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-09 21:56:25 +0200

    fixing bugs

--- src/components/burger/burger-ingredients/burger-ingredients.js
+++ src/components/burger/burger-ingredients/burger-ingredients.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import classes from "./burger-ingredients.module";
+import classes from "./burger-ingredients.module.css";
 import PropTypes from "prop-types";
 
 class BurgerIngredient extends Component {

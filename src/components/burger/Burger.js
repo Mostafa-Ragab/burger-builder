2c74e2b047bf94517e4d4802795c2e5bba6f24f8
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-09 21:56:56 +0200

    adding burger

--- src/components/burger/Burger.js
+++ src/components/burger/Burger.js
@@ -1,9 +1,26 @@
-import Classes from './burger.module.css'
+import classes from "./burger.module.css";
+import BurgerIngredient from "./burger-ingredients/burger-ingredients";
 const Burger = (props) => {
-    const tranformedIngredients = Object.keys(props.ingredients).map(igkey => {...Array(props.ingredients[igkey]).map((-,i) => {
-        return <BurgerIngredients key={igkey + i } type={igkey} /> 
-    })})
+	const tranformedIngredients = Object.keys(props.ingredients)
+		.map((igkey) => {
+			[...Array(props.ingredients[igkey])].map((_, i) => {
+				return <BurgerIngredient key={igkey + i} type={igkey} />;
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
 
-}
+	if (tranformedIngredients.length === 0) {
+		tranformedIngredients = <p>Please start adding ingredients!</p>;
+	}
+	return (
+		<div className={classes.Burger}>
+			<BurgerIngredient type="bread-top" />
+			{tranformedIngredients}
+			<BurgerIngredient type="bread-bottom" />
+		</div>
+	);
+};
 
-export default Burger
+export default Burger;

90ed9399d6717e7bfe431b26dd125aaca9f10509
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-10 15:35:21 +0200

    adding disabled as a prop

--- src/components/burger-controls/build-control/buildControl.js
+++ src/components/burger-controls/build-control/buildControl.js
@@ -3,7 +3,13 @@ import classes from "./buildControl.module.css";
 const BuildControl = (props) => (
 	<div className={classes.BuildControl}>
 		<div className={classes.Label}>{props.label}</div>
-		<button className={classes.Less}>Less</button>
+		<button
+			className={classes.Less}
+			onClick={props.reomved}
+			disabled={props.disabled}
+		>
+			Less
+		</button>
 		<button className={classes.More} onClick={props.added}>
 			More
 		</button>

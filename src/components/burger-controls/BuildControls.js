5894ef34f65e5223dec1a7707e4d6425ea3def0b
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-10 14:09:43 +0200

    adding build controls components

---
+++ src/components/burger-controls/BuildControls.js
@@ -0,0 +1,18 @@
+import classes from "./BuildControls.module.css";
+import BuildControl from "./build-control/buildControl";
+
+const controls = [
+	{ label: "Salad", type: "salad" },
+	{ label: "Bacon", type: "bacon" },
+	{ label: "Cheese", type: "cheese" },
+	{ label: "Meat", type: "meat" },
+];
+const BuildControls = () => (
+	<div className={classes.BuildControls}>
+		{controls.map((ctrl) => (
+			<BuildControl key={ctrl.label} label={ctrl.label} />
+		))}
+	</div>
+);
+
+export default BuildControls;

cfebca8384db6ceb18e6cfbaa73ed9531e36393d
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-11 15:30:07 +0200

    implementing toolbar

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,8 @@
 import "./layout.css";
-
+import Toolbar from "../../components/Navigation/toolbar/Tollbar";
 const Layout = (props) => (
 	<>
+		<Toolbar />
 		<main className="content">{props.children}</main>
 	</>
 );

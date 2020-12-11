454a2e5a2c55b65d017e3c8091c0d9a3d291d9bc
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-11 15:29:52 +0200

    creating navigation

---
+++ src/components/Navigation/toolbar/Tollbar.js
@@ -0,0 +1,11 @@
+import classes from "./Toolbar.module.css";
+
+const Toolbar = (props) => (
+	<header className={classes.Toolbar}>
+		<div>Menu</div>
+		<div>Logo</div>
+		<nav>...</nav>
+	</header>
+);
+
+export default Toolbar;

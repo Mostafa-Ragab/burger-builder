db21d7c57fa3b9e13b3db9fe37a07d09a82c0b5b
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-11 14:54:38 +0200

    implementing backdrop component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,14 +1,19 @@
 import classes from "./Modal.module.css";
+import Aux from "../../../hoc/Aux_";
+import Backdrop from "../Backdrop/Bckdrop";
 const Modal = (props) => (
-	<div
-		className={classes.Modal}
-		style={{
-			transform: props.show ? "translateY(0)" : "translate(-100vh)",
-			opacity: props.show ? "1" : "0",
-		}}
-	>
-		{props.children}
-	</div>
+	<Aux>
+		<Backdrop show={props.show} clicked={props.modalClosed} />
+		<div
+			className={classes.Modal}
+			style={{
+				transform: props.show ? "translateY(0)" : "translate(-100vh)",
+				opacity: props.show ? "1" : "0",
+			}}
+		>
+			{props.children}
+		</div>
+	</Aux>
 );
 
 export default Modal;
